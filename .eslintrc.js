754e1b3722398041fb300b26783f7f93a084e503
Donald Geddes <hi@donaldgeddes.ca>
2020-10-18 15:29:54 -0700

    De-bear the bones

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: false,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  rules: {
+    'arrow-parens': 'off',
+    camelcase: 'off',
+    'import/first': 'off',
+    'import/newline-after-import': 'off',
+    'no-bitwise': 'off',
+    'no-console': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-expressions': [
+      'error',
+      { allowShortCircuit: true, allowTernary: true },
+    ],
+    'no-unused-vars': 'warn',
+    semi: 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['.'],
+      },
+    },
+  },
+}
--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+/* eslint-disable */
+addEventListener('scheduled', event => {
+  event.waitUntil(handleScheduled(event))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+/* eslint-disable */
+
+async function handleScheduled() {
+  return
 }
