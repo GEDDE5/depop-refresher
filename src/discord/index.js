6c5b4157385322f26fb4310fd173f02427ca4fa2
Donald Geddes <hi@donaldgeddes.ca>
2021-03-02 08:57:04 -0800

    Turn Discord Webhook into secret

--- src/discord/index.js
+++ src/discord/index.js
@@ -1,9 +1,9 @@
+require('dotenv').config()
+
 const Webhook = require('./Webhook')
 const template = require('./template')
 
-const WEBHOOK_URL =
-  'https://discord.com/api/webhooks/793658154206494750/NVabttft-zxWfJW3o3mhxVaO1atzKCdELvZfG5HODDvklHZDj3DWxcN-KhYks4ODDVVx'
-const hook = new Webhook(WEBHOOK_URL)
+const hook = new Webhook(process.env.DEPOP_WEBHOOK)
 const { name } = require('../../package.json')
 hook.username = name
 hook.avatarUrl = 'https://i.imgur.com/4i9im75.png'
